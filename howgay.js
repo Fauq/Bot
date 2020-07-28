d3dc3050c3c277639594f0c6371dc12111bd8609
Fauq <68355398+Fauq@users.noreply.github.com>
2020-07-28 11:43:50 -0400

    e

--- howgay.js
+++ howgay.js
@@ -5,11 +5,11 @@ module.exports = {
     description: "this is a howgay command",
     execute(message, args){
 
-        let result = Math.floor((Math.random() * 100));
+        let result = Math.floor(Math.random() * 100);
         let ballembed = new Discord.MessageEmbed()
         .setAuthor(message.author.tag)
         .setColor("#FF9900")
-        .addField("Percent of gay:", result, "%");
+        .addField("Percent of gay:", result);
 
         message.channel.send(ballembed);
 
