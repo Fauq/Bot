7f7efaf7da38d89c6e7625715e0c527e42c9f346
Fauq <68355398+Fauq@users.noreply.github.com>
2020-09-29 10:38:03 -0400

    e

---
+++ commands/poll.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'poll',
+    description: "this is a poll command",
+    execute(message, args){
+        let pollChannel = message.mentions.channels.first();
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+        .setTitle('New Poll!')
+        .setDescription(pollDescription)
+        .setColor('YELLOW')
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
+    }
+
+};
