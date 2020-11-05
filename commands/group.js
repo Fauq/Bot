03c026a079992c1ad8b8bba25405cc2b5ed3d454
Fauq <68355398+Fauq@users.noreply.github.com>
2020-11-04 22:18:16 -0500

    e

--- commands/group.js
+++ commands/group.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'group',
     description: "this is a group command",
     execute(message, args){
-        message.channel.send('Join the following group then say your Roblox username, https://www.roblox.com/groups/7850935/RRC-Group#!/store');
+        message.channel.send('Join the following group then **say your Roblox username**, https://www.roblox.com/groups/7850935/RRC-Group#!/store');
     }
 }
