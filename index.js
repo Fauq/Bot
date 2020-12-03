88c67db5e0bb4187fe7eb66e6b691a5f0fd86795
Fauq <68355398+Fauq@users.noreply.github.com>
2020-12-02 21:30:30 -0500

    e

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ for (const file of commandFiles){
 
 client.on('ready', ()=>{
     console.log("Bot is online.");
-    client.user.setActivity('discord.gg/rrc', { type: 'WATCHING'}).catch(console.error);
+    client.user.setActivity('discord.gg/bobux', { type: 'WATCHING'}).catch(console.error);
 })
 
 client.on('message', (message)=>{
