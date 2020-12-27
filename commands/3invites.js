1994ff5f8cd48e1578229284849a0e0626433235
Fauq <68355398+Fauq@users.noreply.github.com>
2020-12-27 12:56:24 -0500

    e

--- commands/3invites.js
+++ commands/3invites.js
@@ -2,6 +2,6 @@ module.exports = {
     name: '3invites',
     description: "this is a group command",
     execute(message, args){
-        message.channel.send('Make a shirt or gamepass and set the price to 25 Robux');
+        message.channel.send('Make a shirt or gamepass and set the price to 25 Robux.\nNote: The robux will take a few days to unpend and go into your account.');
     }
 }
