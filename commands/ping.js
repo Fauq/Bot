673e717fbaec94cc1f7f879d6e0e36deab489002
Fauq <68355398+Fauq@users.noreply.github.com>
2021-01-03 16:25:54 -0500

    e

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: 'ping',
     description: "this is a ping command",
-    execute(message, args){
-        message.channel.send('pong');
+    execute: (message, args) => {
+        message.channel.send('Calculating ping...').then(resultMessage => {
+            const ping = resultMessage.createdTimestamp - message.createdTimestamp
+
+            message.channel.send(`Bot latency: ${ping}, API Latency: ${client.ws.ping}`)
+    })
+        
     }
 }
