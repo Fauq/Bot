c81b293779da3cc589f8104ca0bbf12c9fcb3665
Haotian <lihao9440@gmail.com>
2021-01-18 21:31:59 -0500

    e

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "*";
 const fs = require('fs');
+const config = require('./config.json');
+client.config = config;
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles){
