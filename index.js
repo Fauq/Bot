9b50a15a125c98bcc0ef4dfc05f7d4714dd59bfb
Haotian <lihao9440@gmail.com>
2021-01-18 21:30:22 -0500

    e

--- index.js
+++ index.js
@@ -14,6 +14,44 @@ client.on('ready', ()=>{
     client.user.setActivity('discord.gg/bobux', { type: 'WATCHING'}).catch(console.error);
 })
 
+// Init discord giveaways
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./database.json",
+    updateCountdownEvery: 3000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+//Coded by Zero
+
+/* Load all events */
+fs.readdir("./events/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`👌 Event loaded: ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
+});
+
+client.commands = new Discord.Collection();
+
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`👌 Command loaded: ${commandName}`);
+    });
+});
+
 client.on('message', (message)=>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
